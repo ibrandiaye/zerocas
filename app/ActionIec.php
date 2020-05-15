069a1ed7b94d5feda883caa391bc9c7b23bd63cf
ibra789ndiaye@gmail.com <ibra789ndiaye@gmail.com>
2020-05-15 23:30:48 +0000

    changement commentaire

--- app/ActionIec.php
+++ app/ActionIec.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ActionIec extends Model
 {
     protected $fillable = [
-        'libelle','desciption','image','user_id'
+        'libelle','commentaire','image','user_id'
     ];
     public function user(){
         return $this->belongsTo(User::class);
